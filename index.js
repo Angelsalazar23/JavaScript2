a6d4fb05dac1238aa573bc013c6306bd436ba00c
Angelsalazar23 <angelitosalazar230597@gmail.com>
2022-10-05 17:56:37 -0500

    Terminado

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+const fechaNacimiento= new Date("may 23 1997")
+const fechaNacimientoAutor=new Date("may 23 1998")
+
+
+const libroFavorito={
+    titulo:"Ventajas de ser invisible ",
+    autor:"Stephen Chbosky",
+    fecha: fechaNacimientoAutor,
+    url:"https://www.amazon.com.mx/Las-Ventajas-Invisible-Stephen-Chbosky/dp/1476732450/ref=asc_df_1476732450/?tag=gledskshopmx-20&linkCode=df0&hvadid=343348915383&hvpos=&hvnetw=g&hvrand=4074017851469788204&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=1010097&hvtargid=pla-569567486610&psc=1"
+}
+
+const lista=["Angel", 25, true, fechaNacimiento,libroFavorito];
+
+console.log(lista);
